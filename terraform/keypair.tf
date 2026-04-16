fe255decbef9f454822daff54c5c042ac9629247
SriRam116 <sriramofficial11006@gmail.com>
2026-04-16 10:26:11 +0530

    file function removed from keypair tf file

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "pipePracKP" {
   key_name   = var.key_name
-  public_key = file(var.public_key)
+  public_key = var.public_key
   depends_on = [aws_security_group.pipePracSG]
 }
